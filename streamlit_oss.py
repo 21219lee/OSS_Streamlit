1488c915002c3690f0deec898eb5e7f96299e822
21219lee <150205311+21219lee@users.noreply.github.com>
2023-11-15 11:48:24 +0900

    Update streamlit_oss.py

--- streamlit_oss.py
+++ streamlit_oss.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("23114485 컴퓨터소프트웨어학부 신한솔")
+#OSS 중간 시험 결과
+OSS_Score = [80, 70, 55, 30, 3, 3, 1, 0]
+
+st.write("23114485 신한솔")
+
+OSS_Score
+
+st.bar_chart(OSS_Score)
